23b197acd4d1d13ce17dc6117fe9862059f0a5c0
seba <sebastian.pieprzyk@gmail.com>
2018-03-08 23:17:05 +0100

    v0.2

--- script/main.js
+++ script/main.js
@@ -1,87 +1,108 @@
-function main (){
-    
-    document.addEventListener('mousemove', function(e){
+function main() {
+
+    /*var test = document.querySelectorAll('.topnav a')
+    for (var i = 0; i < test.length; i++) {
+        test[i].addEventListener('click', function (e) {
+            var temp = e.target.getAttribute('href')
+
+            console.log(temp)
+            document.querySelector('#contact').scrollIntoView({
+                behavior: 'smooth'
+            })
+        })
+    }*/
+    var test = document.querySelector('.picture')
+    test.addEventListener('click', function () {
+        document.querySelector('#contact').scrollIntoView({
+            behavior: 'smooth'
+        })
+    })
+
+
+    document.addEventListener('mousemove', function (e) {
         toggleMenu(e);
     })
-    document.addEventListener('scroll', function(e){
+    document.addEventListener('scroll', function (e) {
         toggleMenuScroll(e);
     })
     var hamburger = document.querySelector('.iconRwd')
-    hamburger.addEventListener('click', function(e){  
-       changeDisplay(e)
+    hamburger.addEventListener('click', function (e) {
+        changeDisplay(e)
         var a = document.querySelectorAll('.topnav a')
-        for (var j = 0; j<a.length; j++){
-        a[j].addEventListener('click', function(e){
-            e.target.classList.remove('topnavRwd')
-        })
-    }
+        for (var j = 0; j < a.length; j++) {
+            a[j].addEventListener('click', function (e) {
+                e.target.classList.remove('topnavRwd')
+            })
+        }
     })
-    
+
     var modal = document.querySelector('.modal')
-    modal.addEventListener('click', function(e){
+    modal.addEventListener('click', function (e) {
         closeModal(e)
-        })
+    })
     var images = document.querySelectorAll('#products .product img')
-    for (var j = 0; j<images.length; j++){
+    for (var j = 0; j < images.length; j++) {
         images[j].addEventListener('click', addModal)
     }
 }
 
-function toggleMenu(e){
+// end of main function //
+
+function toggleMenu(e) {
     var nav = document.querySelector('nav');
     var logo = document.querySelector('.logoNav')
     var windowTopPos = document.documentElement.scrollTop || document.body.scrollTop;
 
-    if (e.clientY>300 && windowTopPos>300){
+    if (e.clientY > 300 && windowTopPos > 300) {
         nav.classList.add('hidden2')
         logo.classList.add('hidden2')
     } else {
-        
+
         nav.classList.remove('hidden2')
         logo.classList.remove('hidden2')
     }
 }
 
-function toggleMenuScroll(e){
+function toggleMenuScroll(e) {
     var windowTopPos = document.documentElement.scrollTop || document.body.scrollTop;
     var nav = document.querySelector('nav');
     var logo = document.querySelector('.logoNav')
-    if (windowTopPos>300){
+    if (windowTopPos > 300) {
         nav.classList.add('hidden2')
         logo.classList.add('hidden2')
     } else {
-        
+
         nav.classList.remove('hidden2')
         logo.classList.remove('hidden2')
-}
+    }
 }
 
-function addModal(e){
+function addModal(e) {
     openModal(e)
 }
 
-function closeModal (e){
+function closeModal(e) {
     var removeNode = e.target.querySelector('.modalGallery')
     console.log(removeNode)
     e.target.removeChild(removeNode)
     e.target.classList.add('hidden')
 }
 
-                               
+
 function changeDisplay(e) {
     e.preventDefault();
     var a = e.target.parentElement.querySelectorAll('a')
     console.log(a)
-    for (var i = 0; i<a.length; i++){
+    for (var i = 0; i < a.length; i++) {
         e.target.parentElement.querySelector('.topnav').style.position = 'relative'
-            a[i].classList.toggle('topnavRwd')
-        }
+        a[i].classList.toggle('topnavRwd')
     }
+}
 
-function hideNav (e) {
+function hideNav(e) {
     var topnaw = e.target.parentElement.querySelectorAll('a')
     console.log(topnaw)
-    for (var i = 0; i<topnaw.length; i++){
+    for (var i = 0; i < topnaw.length; i++) {
         topnaw[i].classList.toggle('topnavRwd')
         console.log(topnaw[i])
     }
@@ -89,9 +110,9 @@ function hideNav (e) {
 }
 
 
-function openModal(e){
+function openModal(e) {
     var section = document.querySelectorAll('section');
-    for (i = 0; i<section.length; i++){
+    for (i = 0; i < section.length; i++) {
         section[i].classList.add('blur');
     }
     var modal = document.querySelector('.modal')
@@ -102,7 +123,7 @@ function openModal(e){
     var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth
     var productName = e.target.parentElement.dataset.product
     console.log(width, mainImageSrc)
-    if (width>600 && productName === 'cora') {
+    if (width > 600 && productName === 'cora') {
         mainImageSrc = insert(mainImageSrc, '_big', 11)
         console.log('bla')
     } else if (width > 600 && productName === 'dorica') {
@@ -111,34 +132,34 @@ function openModal(e){
     mainImage.setAttribute('src', mainImageSrc)
     mainImage.classList.add('mainImage')
     div.appendChild(mainImage)
-    mainImage.addEventListener('click', function(e){
+    mainImage.addEventListener('click', function (e) {
         e.stopPropagation()
     })
-    for (var d = 1; d<4; d++){
+    for (var d = 1; d < 4; d++) {
         var img = document.createElement('img')
         img.setAttribute('src', './images/' + productName + '/' + productName + d + '_.jpg')
-        div.appendChild(img)    
+        div.appendChild(img)
     }
     modal.appendChild(div)
     var smallImages = div.querySelectorAll('img')
-    for (var i = 1; i<smallImages.length; i++){
-        smallImages[i].addEventListener('click', function(e){
+    for (var i = 1; i < smallImages.length; i++) {
+        smallImages[i].addEventListener('click', function (e) {
             e.stopPropagation()
             e.preventDefault()
             var thisAttribute = e.target.getAttribute('src')
-            if (width>600 && productName==='cora'){
-                thisAttribute = insert(thisAttribute,'_big',13)
-                e.target.parentElement.querySelector('.mainImage').setAttribute('src', thisAttribute) 
-            } else if (width>600 && productName==='dorica') {
-                thisAttribute = insert(thisAttribute,'_big',15)
-                e.target.parentElement.querySelector('.mainImage').setAttribute('src', thisAttribute) 
-            } else if (width<600){
-                e.target.parentElement.querySelector('.mainImage').setAttribute('src', thisAttribute) 
+            if (width > 600 && productName === 'cora') {
+                thisAttribute = insert(thisAttribute, '_big', 13)
+                e.target.parentElement.querySelector('.mainImage').setAttribute('src', thisAttribute)
+            } else if (width > 600 && productName === 'dorica') {
+                thisAttribute = insert(thisAttribute, '_big', 15)
+                e.target.parentElement.querySelector('.mainImage').setAttribute('src', thisAttribute)
+            } else if (width < 600) {
+                e.target.parentElement.querySelector('.mainImage').setAttribute('src', thisAttribute)
             }
         })
     }
     modal.classList.remove('hidden')
-    
+
 }
 
 function insert(original, string, place) {
